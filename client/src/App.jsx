53d63cfa5a7ba15cf105a5e3fa9b09cd35f44cab
MAF1344 <muha22126ti@student.nurulfikri.ac.id>
2025-02-11 19:09:36 +0700

    Add some Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,25 @@
+import {Route, Routes} from 'react-router-dom';
+import Home from './pages/students/Home';
+import CoursesList from './pages/students/CoursesList';
+import CourseDetails from './pages/students/CourseDetails';
+import MyEnrollments from './pages/students/MyEnrollments';
+import Player from './pages/students/Player';
+import Loading from './components/students/Loading';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/course-list" element={<CoursesList />} />
+        <Route path="/course-list/:input" element={<CoursesList />} />
+        <Route path="/course/:id" element={<CourseDetails />} />
+        <Route path="/my-enrollments" element={<MyEnrollments />} />
+        <Route path="/player/:courseId" element={<Player />} />
+        <Route path="/loading/:path" element={<Loading />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
