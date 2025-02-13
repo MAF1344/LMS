746f86e265ce9b638aac6c4f905b3d1dfa855190
MAF1344 <muha22126ti@student.nurulfikri.ac.id>
2025-02-13 20:42:12 +0700

    create part of navbar

--- client/src/pages/educators/Educator.jsx
+++ client/src/pages/educators/Educator.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import {Outlet} from 'react-router-dom';
 
 const Educator = () => {
   return (
     <div>
       <h1>Educator Page</h1>
+      <div>{<Outlet />}</div>
     </div>
   );
 };
